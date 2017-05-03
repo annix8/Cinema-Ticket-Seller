dc0a215b38304d83b8c329731a600d8bdf21812f
Mario Yordanov <Mario.Yordanov@pds.nl>
2017-05-03 15:34:13 +0300

    Added Projection controller

---
+++ CinemaTickets.Web/Controllers/ProjectionController.cs
@@ -0,0 +1,17 @@
+﻿using CinemaTickets.DataModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaTickets.Web.Controllers
+{
+    public class ProjectionController : Controller
+    {
+        public ActionResult Index(Movie movie)
+        {
+            return View(movie);
+        }
+    }
+}
