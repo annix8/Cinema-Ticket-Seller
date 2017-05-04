004db59e378ee872e4d655e1b13f25dbdb2bc7d7
mario.yordanov <mario.yordanov@BGWKS-049>
2017-05-04 16:25:31 +0300

    Manager panel now can add halls,movies and projections

--- CinemaTickets.Web/Controllers/ProjectionController.cs
+++ CinemaTickets.Web/Controllers/ProjectionController.cs
@@ -1,4 +1,5 @@
-﻿using CinemaTickets.DataModel.Models;
+﻿using CinemaTickets.DataModel;
+using CinemaTickets.DataModel.Models;
 using CinemaTickets.Services;
 using CinemaTickets.Services.Services;
 using CinemaTickets.Web.ViewModels;
@@ -40,6 +41,46 @@ namespace CinemaTickets.Web.Controllers
                 Movies = allMovies
             };
             return View(model);
+        }
+        [HttpPost]
+        public ActionResult CreateProjection(FormCollection data)
+        {
+            var hallID = int.Parse(data["hallID"]);
+            var movieID = int.Parse(data["movieID"]);
+            var year = int.Parse(data["year"]);
+            var month = int.Parse(data["month"]);
+            var day = int.Parse(data["day"]);
+            var hours = int.Parse(data["hours"]);
+            var minutes = int.Parse(data["minutes"]);
+            var date = new DateTime(year,month,day,hours,minutes,0);
+
+            var context = new CinemaTicketsDbContext();
+            using (context)
+            {
+                var projectionWithSameDate = context.Projections.FirstOrDefault(p => p.TimeOfProjection == date && p.HallID == hallID);
+
+                if (!(projectionWithSameDate == null))
+                {
+                    return new HttpStatusCodeResult(400, "There already is a projection set for that time for this hall");
+                }
+
+                var hallFromDb = context.Halls.FirstOrDefault(h => h.HallID == hallID);
+                var movieFromDb = context.Movies.FirstOrDefault(m => m.MovieID == movieID);
+                if (hallFromDb == null || movieFromDb == null)
+                {
+                    return new HttpStatusCodeResult(400,"Something went wrong! :(");
+                }
+                var projection = new Projection
+                {
+                    Hall = hallFromDb,
+                    Movie = movieFromDb,
+                    TimeOfProjection = date
+                };
+                context.Projections.Add(projection);
+                context.SaveChanges();
+            }
+            
+            return new HttpStatusCodeResult(200,"OK");
         }
     }
 }
